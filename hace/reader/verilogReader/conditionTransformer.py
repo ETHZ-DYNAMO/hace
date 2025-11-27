6a8be7a5bcc34ebf693320087e17abb005ff85be
Carmine Rizzi <crizzi@ethz.ch>
2025-11-27 16:35:29 +0100

    Added verilog reader scripts

---
+++ hace/reader/verilogReader/conditionTransformer.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+# -*- encoding=utf8 -*-
+
+"""
+Author: Hanyu Wang
+Created time: 2024-06-25 23:20:52
+Last Modified by: Hanyu Wang
+Last Modified time: 2024-06-25 23:22:01
+"""
+
+from typing import List, Dict, Any, Tuple
+from lark import Transformer
+from hace.modules import *
+
+
+def flatten(items):
+    result = []
+    for item in items:
+        if isinstance(item, list):
+            result.extend(flatten(item))
+        else:
+            if item is not None:
+                result.append(item)
+    return result
+
+
+class ConditionTransformer(Transformer):
+    def statement(self, items):
+        statements = flatten(items)
+        for statement in statements:
+            assert isinstance(
+                statement, Statement
+            ), f"statement = {statement}, type = {type(statement)}"
+        return statements
+
+    def single_statement(self, items):
+        # we make sure that the return value is a list
+        return flatten(items)
+
+    def statement_block(self, items):
+        return flatten(items)
+
+    # conditional_statement: if_statement else_if_statements? else_statement?
+    def conditional_statement(
+        self, items: List[Tuple[BNode, List[Statement]]]
+    ) -> List[Statement]:
+        allStatements = []
+        currCondition = None
+        for cond, statements in items:
+            # process the condition
+            if currCondition is None:
+                condNew = cond
+            else:
+                condNew = OPNode("!", OPType.UNARY_NOT, currCondition)
+                if cond is not None:
+                    condNew = OPNode("&&", OPType.BINARY_AND, condNew, cond)
+
+            # propagate the condition
+            if currCondition is None:
+                assert cond is not None, "condition is None"
+                currCondition = cond
+            else:
+                currCondition = OPNode("||", OPType.BINARY_OR, currCondition, cond)
+
+            # process the statements
+            for statement in statements:
+                if isinstance(statement, AssignmentStatement):
+                    statement.addCondition(condNew)
+                    # print(f"statement = {statement.assignment.toString()}")
+                allStatements.append(statement)
+
+        return allStatements
+
+    def if_statement(self, items) -> Tuple[BNode, list]:
+        return items
+
+    def else_if_statement(self, items) -> Tuple[BNode, list]:
+        # TODO: we need to consider the previous if statement
+        return items
+
+    def else_statement(self, items) -> Tuple[BNode, list]:
+        return None, items[0]
+
+    # case_statement: "case" "(" expression ")" case_content "endcase"
+    def case_statement(self, items):
+        lhs: BNode = items[0]
+        caseStatements = []
+        currCondition = None
+        for cond, statements in items[1:]:
+            if cond is None:
+                # This is the default case
+                condition = OPNode("!", OPType.UNARY_NOT, currCondition)
+            else:
+                # This is a regular case
+                condition = OPNode("==", OPType.BINARY_EQ, lhs, cond)
+                currCondition = (
+                    condition
+                    if currCondition is None
+                    else OPNode("||", OPType.BINARY_OR, currCondition, condition)
+                )
+
+            # process the statements
+            for statement in statements:
+                if isinstance(statement, AssignmentStatement):
+                    statement.addCondition(condition)
+                caseStatements.append(statement)
+        return caseStatements
+
+    def regular_case(self, items) -> list:
+        return items
+
+    def default_case(self, items) -> list:
+        return None, items[0]
---
+++ hace/reader/verilogReader/portTransformer.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- encoding=utf8 -*-
+
+"""
+Author: Hanyu Wang
+Created time: 2024-06-25 23:50:49
+Last Modified by: Hanyu Wang
+Last Modified time: 2024-06-25 23:58:28
+"""
+
+from lark import Transformer, v_args, Tree
+from hace.modules import *
+
+
+class PortTransformer(Transformer):
+
+    # port_range : "[" expression ":" expression "]"
+    @v_args(inline=True)
+    def port_range(self, start, end):
+        assert isinstance(start, BNode), f"start = {start}, type = {type(start)}"
+        assert isinstance(end, BNode), f"end = {end}, type = {type(end)}"
+        return Range(start, end)
+
+    def variable_list(self, items):
+        return items
+
+    # declaration: [port_direction] [port_type] [signedness] [port_range] variable_list ["=" expression] ";"
+    def declaration(self, items):
+        statements = []
+        dir, t, signedness, range, variables, expression = items
+        dir = PortDirection.fromString(dir.data) if dir is not None else None
+        t = (
+            PortType.fromString(t.data) if t is not None else None
+        )  # NOTE: by default, the type is None to avoid overwriting failure
+        ports = []
+        for variable in variables:
+            port = Port(variable)
+            port.setAll(dir, t, range, False if signedness is None else signedness.data == "signed")
+            if expression:
+                assert (
+                    len(variables) == 1
+                ), "ambiguous assignment for multiple variables"
+                statements.append(
+                    AssignmentStatement(BlockingAssignment(variable, expression))
+                )
+            if not port.isTrivial:
+                ports.append(port)
+        if len(ports) > 0:
+            statements.append(PortDeclaration(ports))
+        return statements
